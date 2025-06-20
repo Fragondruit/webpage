c84acb3b78afd7834a58b2998f360a184b0662d4
Fragondruit <joshnigam99@gmail.com>
2025-06-20 13:14:36 -0500

    Add side menu

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hi, I'm Utkarsh.</h1>
-      </header>
+    <div className="App" style={{ display: 'flex', minHeight: '100vh' }}>
+      <nav className="App-menu">
+        <ul>
+          <li>Background</li>
+          <li>Publications</li>
+          <li>Contributions and Citations</li>
+          <li>Awards</li>
+          <li>Code samples</li>
+          <li>Contact Me!</li>
+        </ul>
+      </nav>
+      <main className="App-content">
+        <header className="App-header">
+          <h1>Hi, I'm Utkarsh.</h1>
+        </header>
+      </main>
     </div>
   );
 }
