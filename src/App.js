79af6072f62014a8e769bab8c3a5a392d156fc92
Fragondruit <joshnigam99@gmail.com>
2025-06-20 13:18:38 -0500

    Add socials

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { SiDevpost } from 'react-icons/si';
 
 function App() {
   return (
@@ -17,6 +19,17 @@ function App() {
       <main className="App-content">
         <header className="App-header">
           <h1>Hi, I'm Utkarsh.</h1>
+          <div className="App-social-icons">
+            <a href="https://www.linkedin.com/in/utkarsh-nigam-49781a168/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <FaLinkedin size={36} />
+            </a>
+            <a href="https://github.com/Fragondruit" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+              <FaGithub size={36} />
+            </a>
+            <a href="https://devpost.com/JoshNigam99" target="_blank" rel="noopener noreferrer" aria-label="Devpost">
+              <SiDevpost size={36} />
+            </a>
+          </div>
         </header>
       </main>
     </div>
